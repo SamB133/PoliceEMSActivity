d823894a73ea8c67af2ab30e83736581a93793e0
SamB133 <64711289+SamB133@users.noreply.github.com>
2026-06-13 02:50:17 -0500

    register nui page and embed script in manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,16 +3,26 @@ game 'gta5'
 
 author 'JaredScar'
 description 'PoliceEMSActivity'
-version '2.0'
+version '2.1'
 url 'https://github.com/JaredScar/PoliceEMSActivity'
+
+-- NUI duty menu
+ui_page 'nui/index.html'
+
+files {
+	'nui/index.html',
+	'nui/style.css',
+	'nui/script.js',
+}
 
 client_scripts {
 	'client.lua',
-    'EmergencyBlips/cl_emergencyblips.lua',
+	'EmergencyBlips/cl_emergencyblips.lua',
 }
 
 server_scripts {
-	'config.lua',
-	"server.lua",
-    'EmergencyBlips/sv_emergencyblips.lua',
+	'config.lua', -- Must load first so Config exists
+	'server.lua',
+	'EmergencyBlips/sv_emergencyblips.lua',
+	'StatusEmbed/sv_statusembed.lua',
 }
